c89556333aae8ce35b6a46688e91a406500a413e
alex leon <alex@leon.st>
2020-11-12 12:44:38 +1100

    Added some new unicorn ignores

--- lint-rulesets/unicorn.js
+++ lint-rulesets/unicorn.js
@@ -4,10 +4,14 @@ module.exports = {
   ],
   "rules": {
     "unicorn/explicit-length-check": "off",
-    "unicorn/filename-case": ["error", { "case": "camelCase" }],
+    "unicorn/filename-case": ["error", {"cases": {"camelCase": true, "pascalCase": true}}],
     "unicorn/prevent-abbreviations": "off",
     "unicorn/prefer-number-properties": "off",
+    "unicorn/prefer-spread": "off",
+    "unicorn/new-for-builtins": "off",
     "unicorn/no-fn-reference-in-iterator": "off",
-    "unicorn/no-nested-ternary": "off"
+    "unicorn/no-nested-ternary": "off",
+    "unicorn/no-null": "off",
+    "unicorn/no-reduce": "off"
   }
 }
