039d3803e9521a50a2f75763c20da9bdb2f9eccc
alex leon <alex@leon.st>
2022-11-30 17:05:47 +1100

    Updated eslint dep to 8.0.0 and updated lint rules to the latest

--- lint-rulesets/flow.js
+++ lint-rulesets/flow.js
@@ -8,7 +8,6 @@ module.exports = {
   },
   "extends": [
     "plugin:flowtype/recommended",
-    "prettier/flowtype"
   ],
   "plugins": [
     "flowtype"
--- lint-rulesets/react.js
+++ lint-rulesets/react.js
@@ -1,7 +1,6 @@
 module.exports = {
   "extends": [
     "plugin:react/recommended",
-    "prettier/react"
   ],
   "plugins": [
     "react",
