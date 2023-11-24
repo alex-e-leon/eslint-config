4177b1c5b751f86db319c2dbfd4f30660759440e
alex leon <alex@leon.st>
2023-11-24 11:25:04 +0100

    Update some base lint rules

--- lint-rulesets/base.js
+++ lint-rulesets/base.js
@@ -8,7 +8,25 @@ module.exports = {
     "prettier"
   ],
   "rules": {
+    "no-plusplus": "off",
     "import/no-named-as-default-member": "off",
-    "import/export": "off"
+    "import/prefer-default-export": "off",
+    "import/export": "off",
+    "no-continue": "off",
+    "no-restricted-syntax": [
+      "error",
+      {
+        "selector": "ForInStatement",
+        "message": "for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries, and iterate over the resulting array."
+      },
+      {
+        "selector": "LabeledStatement",
+        "message": "Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand."
+      },
+      {
+        "selector": "WithStatement",
+        "message": "`with` is disallowed in strict mode because it makes code impossible to predict and optimize."
+      }
+    ]
   }
 }
