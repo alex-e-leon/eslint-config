b1da9b41ec54e99eeafab4aec968df2a4faa893c
alex leon <alex@leon.st>
2025-01-14 22:42:48 +0100

    Setup import rules

--- node-ts.js
+++ node-ts.js
@@ -1,10 +1,12 @@
 import base from './lint-rulesets/base.js';
 import ts from './lint-rulesets/ts.js';
+import importRules from './lint-rulesets/import.js';
 import node from './lint-rulesets/node.js';
 import unicorn from './lint-rulesets/unicorn.js';
 
 export default [
   ...base,
+  ...importRules,
   ...ts,
   ...node,
   ...unicorn,
--- react-ts.js
+++ react-ts.js
@@ -1,11 +1,13 @@
 import base from './lint-rulesets/base.js';
 import ts from './lint-rulesets/ts.js';
 import react from './lint-rulesets/react.js';
+import importRules from './lint-rulesets/import.js';
 import cssModules from './lint-rulesets/css-modules.js';
 import unicorn from './lint-rulesets/unicorn.js';
 
 export default [
   ...base,
+  ...importRules,
   ...ts,
   ...react,
   ...cssModules,
