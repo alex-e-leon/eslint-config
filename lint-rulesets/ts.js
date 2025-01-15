7832a058a8f975ae0e11a7025415db2edf22a1b4
alex leon <alex@leon.st>
2025-01-15 18:17:59 +0100

    Update naming convention and padding-line-between-elements

--- lint-rulesets/ts.js
+++ lint-rulesets/ts.js
@@ -4,6 +4,7 @@ import {
   typescriptExtensions,
   allExtensions,
   allPlusJsonExtensions,
+  getNamingConventionRule,
 } from "./util.js";
 
 export default [
@@ -25,7 +26,7 @@ export default [
       },
     },
     rules: {
-      // import xos typescript space file doesn't actually work, so we need to re-apply the indent rule
+      // Import xos typescript space file doesn't actually work, so we need to re-apply the indent rule
       "@stylistic/indent": ["error", 2, { SwitchCase: 1 }],
 
       // Not included in xo's typesript rules
@@ -38,6 +39,38 @@ export default [
         2,
         { functions: true, classes: true, variables: true },
       ],
+
+      // Overwritten from xo
+      "@typescript-eslint/no-restricted-types": [
+        "error",
+        {
+          types: {
+            object: {
+              message:
+                "The `object` type is hard to use. Use `Record<string, unknown>` instead. See: https://github.com/typescript-eslint/typescript-eslint/pull/848",
+              fixWith: "Record<string, unknown>",
+            },
+            Buffer: {
+              message:
+                "Use Uint8Array instead. See: https://sindresorhus.com/blog/goodbye-nodejs-buffer",
+              suggest: ["Uint8Array"],
+            },
+            "[]": "Don't use the empty array type `[]`. It only allows empty arrays. Use `SomeType[]` instead.",
+            "[[]]":
+              "Don't use `[[]]`. It only allows an array with a single element which is an empty array. Use `SomeType[][]` instead.",
+            "[[[]]]": "Don't use `[[[]]]`. Use `SomeType[][][]` instead.",
+            "[[[[]]]]": "ur drunk 🤡",
+            "[[[[[]]]]]": "🦄💥",
+          },
+        },
+      ],
+      ...getNamingConventionRule({ isTsx: false }),
+    },
+  },
+  {
+    files: ["**/*.tsx"],
+    rules: {
+      ...getNamingConventionRule({ isTsx: true }),
     },
   },
 ];
